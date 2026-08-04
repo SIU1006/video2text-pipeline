925191e71659fa92b82019646ea4f34e47e24c87
SIU <borissiu1006@gmail.com>
2026-08-04 22:58:07 +1000

    fix: prevent empty window between checking cache & subscribe (#1)
    In the original design, the websocket checks for stored result cache in Redis first, then do the subscription to listen.
    
    Between checking stored cache and subscription, there's a window that might miss tasks that are completed during that time. Same case for the window between the publish first then setex structure in celery.
    
    Therefore, websocket now subscribe first then check cache to prevent that empty window. Same for publish and setex, setex first then publish.

--- app/routes/websocket.py
+++ app/routes/websocket.py
@@ -10,16 +10,17 @@ async def websocket_endpoint(websocket: WebSocket, task_id: str):
     await websocket.accept()
     r = redis.Redis.from_url(os.getenv("BROKER_URL", "redis://localhost:6379/0"))  
 
-    # Check for stored result in Redis before subscribe
+    # Need to sub first before checking cache to avoid race condition
+    pubsub = r.pubsub()
+    pubsub.subscribe(f"task:{task_id}")
+
+    # Check cache in Redis
     stored = r.get(f"result:{task_id}")
     if stored:
         await websocket.send_text(stored.decode("utf-8"))  
         await websocket.close()
         return
 
-    # Otherwise normal sub
-    pubsub = r.pubsub()
-    pubsub.subscribe(f"task:{task_id}")
 
     loop = asyncio.get_event_loop() # Separate thread
     data = await loop.run_in_executor(None, wait_for_message, pubsub)
