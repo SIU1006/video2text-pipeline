f734a57b8e9f7c6eedea93b15c14b9fbe80e232f
SIU <borissiu1006@gmail.com>
2026-08-17 12:11:10 +1000

    fix: celery-config (#6)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import os
 
 os.environ.setdefault("BROKER_URL", "redis://redis-service:6379/0")
-os.environ.setdefault("BACKEND_URL", "redis://redis-service:6379/0")
 os.environ.setdefault("WHISPER_URL", "http://whisper-service:3000")
 os.environ.setdefault("OLLAMA_HOST", "http://ollama-service:11434")
 os.environ.setdefault("LLM_MODEL", "llama3.2")
--- worker/celery_app.py
+++ worker/celery_app.py
@@ -4,17 +4,17 @@ import os
 
 load_dotenv() # Load .env into os env
 assert os.getenv("BROKER_URL") is not None, "BROKER_URL is not set in .env"
-assert os.getenv("BACKEND_URL") is not None, "BACKEND_URL is not set in .env"
 
 
 celery_app = Celery(
     "worker",
     broker=os.getenv("BROKER_URL"),
-    backend=os.getenv("BACKEND_URL")
 )
 
 celery_app.conf.update(
     task_soft_time_limit=1800, # Raise Exception
     task_time_limit=1900, # force-kill
     task_acks_late=True, # Redis will re-queue the force-killed tasks
+    worker_prefetch_multiplier=1, # 1 worker 1 task
+    broker_connection_retry_on_startup=True, # prevent worker crashing if redis is not ready yet
 )
