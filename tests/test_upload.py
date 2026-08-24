52b079a1f4ef9e431d11477ebdb4a6d8a9212824
borissiu1006 <borissiu1006@gmail.com>
2026-08-24 21:20:45 +1000

    feat: added more tests

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,5 +1,5 @@
 import io
-from unittest.mock import patch
+from unittest.mock import patch, AsyncMock # MagicMock for async
 
 from fastapi.testclient import TestClient
 
@@ -8,13 +8,13 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_upload_returns_taskid():
-    fake_file = io.BytesIO(b"fake video content")
+def test_returns_taskid():
+    file = io.BytesIO(b"fake video content")
     with patch(
         "app.routes.upload.process_video.delay"
     ) as mock_delay:  # Mock Celery task so no need Redis
         response = client.post(
-            "/api/v1/upload", files={"file": ("test.mp4", fake_file, "video/mp4")}
+            "/api/v1/upload", files={"file": ("test.mp4", file, "video/mp4")}
         )
     assert response.status_code == 200
     data = response.json()
@@ -23,6 +23,19 @@ def test_upload_returns_taskid():
     mock_delay.assert_called_once()  # Ensure the Celery task was called
 
 
-def test_upload_no_file_returns_422():
+def test_no_file():
     response = client.post("/api/v1/upload")
     assert response.status_code == 422
+
+def test_file_size(isolated_upload_dir, monkeypatch):
+    monkeypatch.setattr("app.routes.upload.MAX_SIZE", 10) # Set max size as 10bytes for this test
+    file = io.BytesIO(b"This line is already way more than 10 bytes")
+
+    response = client.post(
+        "/api/v1/upload", files={"file": ("test.mp4", file, "video/mp4")}
+    )
+
+    assert response.status_code == 413
+    assert "1GB" in response.json()["detail"]
+
+    assert list(isolated_upload_dir.iterdir()) == [] # Check if file is cleaned up
